42ebf53c103484d683bbfaeedc0e33a02c090435
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-04-07 19:02:31 +0530

    add views

--- deploy/riba/urls.py
+++ deploy/riba/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view(), name='main'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^chatterbot/', include('chatterbot.ext.django_chatterbot.urls', namespace='chatterbot')),
+    url(r'^appointment.html', appointment.as_view(), name='appointment'),
+    url(r'^contact.html', contact.as_view(), name='contact'),
+    url(r'^doctor.html', doctor.as_view(), name='doctor'),
 ]
--- deploy/riba/views.py
+++ deploy/riba/views.py
@@ -4,3 +4,12 @@ from django.views.generic.base import TemplateView
 class ChatterBotAppView(TemplateView):
     template_name = "app.html"
 
+class appointment(TemplateView):
+    template_name = "appointment.html"
+
+class contact(TemplateView):
+    template_name = "contact.html"
+
+class doctor(TemplateView):
+    template_name = "doctor.html"
+
